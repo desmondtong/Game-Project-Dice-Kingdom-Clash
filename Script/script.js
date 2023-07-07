38b379a254890eb31e3e08afcea1bf643a6960bc
Desmond Tong <tysdesmond2@gmail.com>
2023-07-07 18:35:50 +0800

    Add overlay in html and event button

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,15 @@
 const body = document.querySelector("body");
 const startMenu = document.querySelector(".start-menu");
 const gameScreen = document.querySelector(".game-screen");
+const overlay = document.querySelector(".overlay");
+
 const startBtn = document.querySelector(".start-btn");
 const tutoBtn = document.querySelector(".tutorial-btn");
 
 startBtn.addEventListener("click", function () {
-  startMenu.classList.add("hidden");
-  gameScreen.classList.remove("hidden");
-  body.style.backgroundImage = "url('')";
+  overlay.classList.remove("hidden");
+});
+
+tutoBtn.addEventListener("click", function () {
+  overlay.classList.remove("hidden");
 });
