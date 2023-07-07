e398b63a72c0002a0618618aef144a5c708043d6
Desmond Tong <tysdesmond2@gmail.com>
2023-07-07 23:11:22 +0800

    Replace manual overlay with Bootstrap modal window

--- Script/script.js
+++ Script/script.js
@@ -1,15 +1,3 @@
 const body = document.querySelector("body");
 const startMenu = document.querySelector(".start-menu");
 const gameScreen = document.querySelector(".game-screen");
-const overlay = document.querySelector(".overlay");
-
-const startBtn = document.querySelector(".start-btn");
-const tutoBtn = document.querySelector(".tutorial-btn");
-
-startBtn.addEventListener("click", function () {
-  overlay.classList.remove("hidden");
-});
-
-tutoBtn.addEventListener("click", function () {
-  overlay.classList.remove("hidden");
-});
