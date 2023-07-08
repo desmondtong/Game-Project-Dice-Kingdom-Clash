a15f9d5d1555bb77e54e0e1c57e4d8abe9480290
Desmond Tong <tysdesmond2@gmail.com>
2023-07-08 15:45:09 +0800

    Complete start-game-modal layout

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,16 @@
 const body = document.querySelector("body");
 const startMenu = document.querySelector(".start-menu");
 const gameScreen = document.querySelector(".game-screen");
+const playBtn = document.querySelector("#startGameModal .btn-primary");
+
+const p1Name = document.querySelector("#p1Name");
+const p2Name = document.querySelector("#p2Name");
+
+playBtn.addEventListener("click", function () {
+  startMenu.classList.add("hidden");
+  body.style.background = "url(' ')";
+
+  gameScreen.classList.remove("hidden");
+
+  gameScreen.innerText = `${p1Name.value} VS ${p2Name.value}`;
+});
